48818145de9bd9edcf7a6be75a480eaf28f83752
kmoon4751 <mg_cha@naver.com>
2023-11-06 12:04:07 +0900

    '버전1'

---
+++ myapp/src/main/java/myapp/myapp/HomeController.java
@@ -0,0 +1,13 @@
+package myapp.myapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+  @GetMapping
+  public String home(){
+    return "Home/home";
+  }
+}
